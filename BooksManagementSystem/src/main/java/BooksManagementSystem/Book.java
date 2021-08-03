8ef19d769537e7d9ab3e1a24fe46113640723bf9
BaoDQ <dqb2631996@gmail.com>
2021-08-03 23:19:10 +0700

    Bai 3 Cau 1

---
+++ BooksManagementSystem/src/main/java/BooksManagementSystem/Book.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BooksManagementSystem;
+
+/**
+ *
+ * @author mac
+ */
+public class Book implements IBook {
+    int id;
+    String name;
+    String publishDate;
+    String author;
+    String language;
+    float averagePrice;
+
+    @Override
+    public void display() {
+    }
+    
+    
+}
